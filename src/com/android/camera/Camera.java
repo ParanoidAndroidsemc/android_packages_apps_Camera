64b0ec09370f505268023980c8dabaf72f43265a
KalimochoAz <calimochoazucarado@gmail.com>
2012-06-15 17:30:54 +0200

    Add option to remove sound on focus camera
    PATCH 2: Correct to make this setting avoid need to restart camera to work
    
    Change-Id: I9a04c8fd2cbaf08a50eb00347e3e3b8897b00720

--- src/com/android/camera/Camera.java
+++ src/com/android/camera/Camera.java
@@ -1211,7 +1211,9 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
 
     @Override
     public void playSound(int soundId) {
-        mCameraSound.playSound(soundId);
+        if(focusSound(mPreferences)) {
+            mCameraSound.playSound(soundId);
+        }
     }
 
     private boolean saveDataToFile(String filePath, byte[] data) {
@@ -1404,6 +1406,7 @@ public class Camera extends ActivityBase implements FocusManager.Listener,
         final String[] OTHER_SETTING_KEYS = {
                 CameraSettings.KEY_RECORD_LOCATION,
                 CameraSettings.KEY_POWER_SHUTTER,
+                CameraSettings.KEY_FOCUS_SOUND,
                 CameraSettings.KEY_STORAGE,
                 CameraSettings.KEY_PICTURE_SIZE,
                 CameraSettings.KEY_FOCUS_MODE,
